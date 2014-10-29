ca462d4af41324e5085c6d4ed9b7421471753249
Volodymyr <tovsvit@gmail.com>
2014-10-29 15:52:28 +0100

    First commit with two files

---
+++ script_hw.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+#!/usr/bin/ruby1.9.3
+
+# script_hw.rb Version 1.0.1 on 2014-10-22
+
+version = '1.0.1'
+
+puts "Hello World. Ruby script version=#{version} for regular Ruby, not for new Ruby (1.9.x)."
+
+item = 1
+
+2.upto(4) do |item|
+  p item
+end
+
+
+puts "If item = 4, then Ruby 1.8.x, if item = 1, then Ruby 1.9.x, item=#{item}"
+
---
+++ script_hw.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# script_hw.sh Version 1.0 on 2014-10-22
+
+
+version="1.0"
+
+echo "Hello World. Shell script version=${version}"
+
+echo "Waiting some seconds for next script ..."
+sleep 10
+
+./script_hw.rb
+
